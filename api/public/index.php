a204f0f83bcc2378fa472b1eaa22cc047dfa016b
alex <ssokoloss1@gmail.com>
2025-09-20 18:35:46 +0300

    added split config

--- api/public/index.php
+++ api/public/index.php
@@ -2,28 +2,19 @@
 
 declare(strict_types=1);
 
-use App\Http;
-use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Container\ContainerInterface;
 use Slim\Factory\AppFactory;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$builder = new DI\ContainerBuilder();
-
-$builder->addDefinitions([
-    'config' => [
-        'debug' => (bool)getenv('APP_DEBUG'),
-    ],
-]);
-
-$container = $builder->build();
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
 
 $app = AppFactory::createFromContainer($container);
 
-$app->addErrorMiddleware($container->get('config')['debug'], true, true);
-
-$app->get('/', Http\Action\HomeAction::class);
+(require __DIR__ . '/../config/middleware.php')($app, $container);
+(require __DIR__ . '/../config/routes.php')($app);
 
 $app->run();
