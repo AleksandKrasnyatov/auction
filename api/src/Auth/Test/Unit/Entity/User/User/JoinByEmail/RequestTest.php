bf1a480e24280c1ea37df1289b51f54823d543a2
alex <ssokoloss1@gmail.com>
2025-11-04 19:08:15 +0300

    added raw use case for joining by email

---
+++ api/src/Auth/Test/Unit/Entity/User/User/JoinByEmail/RequestTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Test\Unit\Entity\User\User\JoinByEmail;
+
+use App\Auth\Entity\User\Email;
+use App\Auth\Entity\User\Id;
+use App\Auth\Entity\User\Token;
+use App\Auth\Entity\User\User;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * covers User
+ */
+class RequestTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $user = new User(
+            $id = Id::generate(),
+            $date = new DateTimeImmutable(),
+            $email = new Email('user@mail.ru'),
+            $hash = 'hash',
+            $token = new Token(Uuid::uuid4()->toString(), new DateTimeImmutable())
+        );
+
+        self::assertSame($id, $user->getId());
+        self::assertSame($date, $user->getDate());
+        self::assertSame($email, $user->getEmail());
+        self::assertSame($hash, $user->getPasswordHash());
+        self::assertSame($token, $user->getJoinConfirmToken());
+    }
+}
